4c76e5bac9271538f1124340be580c521e283cb9
TomTheToad <tomthetoad951@gmail.com>
2018-11-04 07:19:26 -0500

    Continuing experimentation with NSFetchedResultsController, sections, and relationships.

--- SimpleTaskList/Controller/CoreDataController.swift
+++ SimpleTaskList/Controller/CoreDataController.swift
@@ -19,8 +19,101 @@ class CoreDataController: NSObject {
         return appDelegagte.persistentContainer.viewContext
     }()
     
-    // Test func
-    func getTaskEntity() -> Task {
-        return Task(context: context)
+    // Default group id
+    let defaultGroupId: UUID = {
+        // TODO set this
+        return UUID()
+    }()
+    
+    override init() {
+        super.init()
+    }
+    
+    // Create
+    // 1) Establish new Task entity,
+    // 2) Associate with first group in which the name matches or set default
+    // 3) Save context
+    // 4) Return Task
+    @discardableResult
+    func newTask(name: String, info: String, groupId: UUID?) -> Task {
+        let newTask =  Task(context: context)
+        newTask.id = UUID()
+        newTask.name = name
+        newTask.info = info
+        newTask.dateCreated = Date()
+        
+        guard let id = groupId else {
+            // TODO: use / create / get default group id
+            newTask.id = defaultGroupId
+            saveContext()
+            return newTask
+        }
+        
+        newTask.id = id
+        return newTask
+    }
+    
+    func newTaskGroup(name: String, info: String?) -> TaskGroup {
+        let newTaskGroup = TaskGroup(context: context)
+        newTaskGroup.id = UUID()
+        newTaskGroup.name = name
+        
+        guard let info = info else {
+            saveContext()
+            return newTaskGroup
+        }
+        
+        newTaskGroup.info = info
+        return newTaskGroup
+    }
+    
+    // Read
+    func getAllTaskGroups() -> NSFetchedResultsController<TaskGroup> {
+        // TODO: set default request in core data?
+        let request = NSFetchRequest<TaskGroup>(entityName: "TaskGroup")
+        let sortName = NSSortDescriptor(key: #keyPath(TaskGroup.name), ascending: true)
+        let sortId = NSSortDescriptor(key: #keyPath(TaskGroup.id), ascending: true)
+        
+        request.sortDescriptors = [sortName, sortId]
+        let fetchedResultsController = NSFetchedResultsController(fetchRequest: request, managedObjectContext: context, sectionNameKeyPath: #keyPath(TaskGroup.name), cacheName: nil)
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {
+            print("Unable to perform petch")
+        }
+        return fetchedResultsController
+    }
+    
+    func getAllTasks() -> NSFetchedResultsController<Task> {
+        let request = NSFetchRequest<Task>(entityName: "Task")
+        let sort = NSSortDescriptor(key: #keyPath(Task.belongsToGroup.name), ascending: true)
+        request.sortDescriptors = [sort]
+        let fetchedResultsController = NSFetchedResultsController(fetchRequest: request, managedObjectContext: context, sectionNameKeyPath: #keyPath(TaskGroup.name), cacheName: nil)
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {
+            print("Unable to perform fetch")
+        }
+        return fetchedResultsController
+    }
+    
+    // Update
+    
+    // Delete
+    
+    // helpers
+    func getDefaultGroupId() {
+        // TODO: check for default group
+    }
+    
+    func saveContext() {
+        if context.hasChanges {
+            do {
+                try context.save()
+                print("context saved")
+            } catch {
+                print("Problem saving context")
+            }
+        }
     }
 }
--- SimpleTaskList/Controller/MainVC.swift
+++ SimpleTaskList/Controller/MainVC.swift
@@ -7,8 +7,24 @@
 //
 
 import UIKit
+import CoreData
 
 class MainVC: UIViewController {
+    // Fields
+    let coreData = CoreDataController()
+//    var fetchedRC: NSFetchedResultsController<Task>! {
+//        didSet {
+//            print("fetchedRC was set")
+//        }
+//    }
+    var fetchedRCGroups: NSFetchedResultsController<TaskGroup>! {
+        didSet {
+            print("fetchedRCGroups was set")
+        }
+    }
+    
+    // Current task group for table section
+    var currentTaskGroup: TaskGroup?
     
     // IBOutlets
     @IBOutlet weak var tableView: UITableView!
@@ -16,29 +32,85 @@ class MainVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+//        fetchedRC = coreData.getAllTasks()
+        fetchedRCGroups = coreData.getAllTaskGroups()
+        addTestItems()
 
         tableView.delegate = self
         tableView.dataSource = self
+//        fetchedRC.delegate = self
+        fetchedRCGroups.delegate = self
         
         newTaskButton.layer.cornerRadius = 5
+//        updateFetchedResultsGroups()
+    }
+    
+    // IBActions
+    @IBAction func newTaskButton(_ sender: Any) {
+//        let group = fetchedRC.fetchedObjects?.first!
+//        coreData.newTask(name: "a new task", info: "test task", groupId: group?.id!)
+    }
+    
+    // Helper Functions
+//    func updateFetchedResults() {
+//        do {
+//            try fetchedRC.performFetch()
+//        } catch {
+//            print("problem with task fetch")
+//        }
+//        tableView.reloadData()
+//    }
+    
+    func updateFetchedResultsGroups() {
+        do {
+            try fetchedRCGroups.performFetch()
+        } catch {
+            print("problem with group fetch")
+        }
     }
 
 }
 
 extension MainVC: UITableViewDelegate, UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        let count = fetchedRCGroups.fetchedObjects?.count ?? 0
+//        print("number of sections: \(count)")
+        return count
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 4
+        guard let section = fetchedRCGroups.fetchedObjects?[section] else {
+            return 0
+        }
+        
+        print("using section: \(String(describing: section.name))")
+        
+        // MARK: test set current task group
+        currentTaskGroup = section
+        
+        guard let numberTasks = section.hasTasks?.count else {
+            return 0
+        }
+//        print("number of task in section: \(numberTasks)")
+        return numberTasks
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "tableCell") else {
             return UITableViewCell()
         }
-        cell.textLabel?.text = "New Task"
+        
+//        guard let taskGroup = currentTaskGroup else {
+//            return cell
+//        }
+        
+        guard let taskGroup = fetchedRCGroups.sections[]
+        
+        guard let task = taskGroup.hasTasks?.allObjects[indexPath.row] as? Task else {
+            return cell
+        }
+        
+        cell.textLabel?.text = task.name
         return cell
     }
     
@@ -52,6 +124,19 @@ extension MainVC: UITableViewDelegate, UITableViewDataSource {
         let view = UIView(frame: frame)
         let color = UIColor(red: 82/255, green: 174/255, blue: 1, alpha: 1)
         view.backgroundColor = color
+        
+        let label = UILabel()
+        
+        view.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
+        label.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 0).isActive = true
+        
+        guard let taskGroup = currentTaskGroup else {
+            label.text = "Title Missing"
+            return view
+        }
+        label.text = taskGroup.name!
         return view
     }
     
@@ -61,5 +146,52 @@ extension MainVC: UITableViewDelegate, UITableViewDataSource {
         }
         
         cell.textLabel?.textColor = (cell.textLabel?.textColor == .lightGray) ? .black : .lightGray
+    }
+}
+
+extension MainVC: NSFetchedResultsControllerDelegate {
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        
+        let index = indexPath ?? (newIndexPath ?? nil)
+        guard let cellIndex = index else {
+            return
+        }
+        
+        switch type {
+        case .insert:
+            tableView.insertRows(at: [cellIndex], with: .automatic)
+            print("new object detected")
+        case .delete:
+            tableView.deleteRows(at: [cellIndex], with: .automatic)
+        default:
+            break
+        }
+    }
+}
+
+extension MainVC {
+    // Test Functions
+    func addTestItems() {
+        // Test groups which hold tasks (aka group of tasks)
+        let testGroup1 = coreData.newTaskGroup(name: "testGroup1", info: nil)
+        let testGroup2 = coreData.newTaskGroup(name: "testGroup2", info: nil)
+        
+        // Tasks group 1
+        let task1 = coreData.newTask(name: "task1", info: "do something", groupId: testGroup1.id)
+        let task2 = coreData.newTask(name: "task2", info: "do another thing", groupId: testGroup1.id)
+        
+        // Tasks group 2
+        let task3 = coreData.newTask(name: "task3", info: "do a bigger thing", groupId: testGroup2.id)
+        let task4 = coreData.newTask(name: "task4", info: "do a small thing", groupId: testGroup2.id)
+        
+        // Add tasks to group 1
+        testGroup1.addToHasTasks(task1)
+        testGroup1.addToHasTasks(task2)
+        
+        // Add tasks to group 2
+        testGroup2.addToHasTasks(task3)
+        testGroup2.addToHasTasks(task4)
+        
+        coreData.saveContext()
     }
 }
